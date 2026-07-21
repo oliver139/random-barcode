e4c5644f8f5ff930358c1b20b0a532e3f1e9d7c8
Oliver Mak <oliver139.working@gmail.com>
2026-07-22 00:44:38 +0800

    Deployment workflow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'modern-normalize'
+import 'modern-normalize/modern-normalize.css'
 import '@/assets/main.css'
 
 createApp(App).mount('#app')
